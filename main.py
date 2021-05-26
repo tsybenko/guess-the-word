bc9f05d5bdfce414b70b05f9d899bcd8128b6cfd
tsybenko <tsybenko.dmitry@gmail.com>
2021-05-26 22:41:27 +0300

    - add score system - add game repeat ability - refactoring

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import random
 import art
 import json
 import termcolor
+import terminal
 
-app_name = 'Guess The Word'
-
-
-def get_header():
-    return art.text2art(app_name)
+alphabet = [
+    'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h',
+    'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x',
+    'y', 'z'
+]
 
 
 def get_words():
@@ -18,37 +23,52 @@ def get_words():
     return json_data
 
 
-def start_game():
-    is_running = True
+def get_random_word():
     words = get_words()
+    return random.choice(words)
 
-    print(get_header())
 
-    word = random.choice(words)
+def start_game(score):
+    is_running = True
+    is_win = False
+    word = get_random_word()
     attempted_letters = []
     display = ['_' for _ in word]
-    attempts = math.ceil(len(word) / 2)
+    attempts_available = math.ceil(len(word) / 2)
 
     # print(f'The word is - {word}')
 
     while is_running:
         if ''.join(display) == word:
-            print(f'It\'s "{word}"')
-            print(termcolor.colored('You win!', 'green'))
+            print(f'\nIt\'s "{word}"')
+            terminal.success('You win!')
+            is_win = True
             break
 
-        if attempts < 1:
-            print(termcolor.colored('Game over!', 'red'))
-            print(f'The word was - "{word}"')
+        if attempts_available < 1:
+            terminal.error('Game over!')
+            print(f'It was "{word}" word')
             break
 
-        print(f'Attempts: {attempts}')
+        print(f'Attempts remaining: {attempts_available}')
         print(''.join(display))
 
         if len(attempted_letters) > 0:
             print('Letters attempted: ' + ', '.join(attempted_letters))
 
         input_letter = input('Guess a letter: ').lower()
+
+        # if input_letter == word:
+        #     score += 50
+        #     terminal.success('BINGO!')
+        #     break
+
+        if len(input_letter) > 1:
+            terminal.warn('Only 1 letter could be passed')
+            continue
+        elif input_letter not in alphabet:
+            terminal.warn('The value must be an alphabetical letter')
+            continue
 
         letter_guessed = False
         if input_letter in word:
@@ -58,15 +78,39 @@ def start_game():
                 if input_letter == letter:
                     letter_guessed = True
                     display[pos] = letter
+                    score += 1
         else:
-            attempts -= 1
+            attempts_available -= 1
 
         colored_letter = termcolor.colored(input_letter, 'green' if letter_guessed else 'red')
         if colored_letter not in attempted_letters:
             attempted_letters.append(colored_letter)
 
-    return 0
+    return {
+        "is_win": is_win,
+        "attempted_letters": len(attempted_letters),
+        "bad_attempts": len(attempted_letters) - len(word),
+        "score": score
+    }
 
 
 if __name__ == '__main__':
-    start_game()
+    art.tprint('Guess The Word')
+
+    win_count = 0
+    points = 0
+    repeat = True
+    while repeat:
+        result = start_game(points)
+
+        if result["is_win"]:
+            win_count += 1
+            points = result["score"]
+
+        if win_count > 1:
+            points *= 2
+
+        answer = input('\nDo you want to retry? [Y/N]\n')
+        repeat = True if answer.lower() == 'y' else False
+
+    print(f'You have {points} points. Thank you for game!')
